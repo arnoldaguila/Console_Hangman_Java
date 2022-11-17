34259a84c83cd172ba7045885aefd8fa0c155eaf
arnoldaguila04 <arnoldaguila04@gmail.com>
2022-11-17 14:01:27 -0500

    Updated date, updated line 18, 37, 40, 143, 147, 155, 156, 157, ,159, 160, 164, 169, 171 (added recursion), 177, 178, 180, 188, 195 (added return), 200, 201 (added return), 214 - 217 (Doc comments for win()), 221 - 236 (new lines of code), 239 - 267 (new lines of code), 276 (replaced user_input.replace(" ", "") with user_input.replace("\\s", "")), 279 - 285 (Doc comments).

--- src/ConsoleHangman.java
+++ src/ConsoleHangman.java
@@ -10,12 +10,12 @@ import java.util.Scanner;
  * ConsoleHangman
  * This is the ConsoleHangman class it contains a static main and getWord methods.
  * @author Arnold Aguila
- * @version 11/8/22
+ * @version 11/17/22
  */
 public class ConsoleHangman {
     public static void main(String[] args) {
         gameMessage(); // running game message
-        game(getWord("word_bank")); // running game.
+        game(); // running game.
     }
 
     /**
@@ -34,10 +34,10 @@ public class ConsoleHangman {
         }
         ArrayList<String> wordArray = new ArrayList<>();
         while(input.hasNextLine()){
-            wordArray.add(input.nextLine());
+            wordArray.add(input.nextLine()); // reading file and adding to an ArrayList
         }
         input.close();
-        return wordArray.get(randInt.nextInt(0, wordArray.size()));
+        return wordArray.get(randInt.nextInt(0, wordArray.size())); // returning a random word from the ArrayList.
     }
 
     /**
@@ -140,70 +140,65 @@ public class ConsoleHangman {
         System.out.println();
     }
 
-    public static void game(String word){
+    public static void game(){
         Scanner userInput = new Scanner(System.in);
         ArrayList<String> guessedLettersBefore = new ArrayList<>(); // Array for the guessed letters
         int chances = 6; // number of body parts and chances.
-        String secretWord = word;
+        String secretWord = getWord("word_bank.txt");
         secretWord = cleanUp(secretWord);
         LinkedList<String> blankLinesArray = new LinkedList<>();
         for(int i = 0; i < secretWord.length(); i++){//adding blank lines for the number of letters in the
             blankLinesArray.add("_");                //secret word.
         }
 //        System.out.println("Not supposed to be here: " + secretWord); //HERE FOR TESTING PURPOSE.
-        while(true) {
-            guessedLettersArray(guessedLettersBefore);
+
+        while(true){
+            System.out.println();
+            guessedLettersArray(guessedLettersBefore); // printing out the letters that are guessed.
             hangmanPrint(chances); // prints the gallow and the hangman.
-            System.out.println(blankLinesPrint(blankLinesArray));
-            System.out.println("Chances left: " + chances);
+            System.out.println(blankLinesPrint(blankLinesArray)); // printing out the blank lines
+            System.out.println("Chances left: " + chances); // printing out how many guesses the user has left.
             System.out.println();
             System.out.print("Enter a letter: ");
             String input = userInput.nextLine();
-            input = input.toLowerCase();
-            input = input.replaceAll("\\s", "");
+            input = cleanUp(input);
             if (input.equals("")) {
                 System.out.println("Error: You didn't enter a letter.");
             } else if (input.length() > 1) { //if input was more than one letter
                 if (input.equals("end")) { //checking to see if the input is end.
-                    endSwitch = true;
-                    break;
+                    return;
                 } else if (input.equals("restart")) { //checking to see if input is restart.
-
-                    break;
+                    game();
                 } else { // if neither end | restart print error message.
                     System.out.println("Error: Can't input more then 1 letter.");
                 }
             } else {
                 int ascii = input.charAt(0);
-                if ((ascii < 97) || (ascii > 122)) {
-                    System.out.println("Error invalid input. please enter a letter.");
+                if ((ascii < 97) || (ascii > 122)) { // checking to see if the input is a letter.
+                    System.out.println("Error invalid input. Please enter a letter.");
                 } else {
-                    if (guessedLettersBefore.contains(input)) { //if letter is in the guessedLettersBefore array.
+                    if (guessedLettersBefore.contains(input)) { //if letter has been guessed before.
                         System.out.println("You already guess that letter.");
-                        chances = chances - 1;
-                        if (chances == 0) {
-                            loss();
-                            break;
-                        }
-                    } else {
+                    }else{
                         if (secretWord.contains(input)) { //if letter is in the secretWord
                             guessedLettersBefore.add(input);
                             for (int i = 0; i < input.length(); i++) {
                                 for (int j = 0; j < secretWord.length(); j++) {
                                     if (input.charAt(i) == secretWord.charAt(j)) {
-                                        blankLinesArray.set(j, input);
+                                        blankLinesArray.set(j, input); // updating blank lines when the user guesses correctly
                                     }
                                 }
                             }
                             System.out.println();
                             if (blankLinesPrint(blankLinesArray).equals(secretWord)) {
                                 win();
-                                break;
+                                return;
                             }
                         } else {
                             chances = chances - 1;
                             if (chances == 0) {
-                                break;
+                                loss(secretWord);
+                                return;
                             } else {
                                 guessedLettersBefore.add(input);
                                 System.out.println();
@@ -213,17 +208,62 @@ public class ConsoleHangman {
                 }
             }
         }
-
     }
 
 
-
+    /**
+     * win()
+     * This static method prints a win message, then asks the user if they want to play again.
+     */
     public static void win(){
         Scanner user_input = new Scanner(System.in);
         System.out.println("You won!");
-        System.out.print("Do you want to play again?");
-        String answer = user_input.next();
-        answer = cleanUp(answer);
+        while(true){
+            System.out.print("Do you want to play again? Yes or no: ");
+            String answer = user_input.next();
+            answer = cleanUp(answer);
+            if (answer.equals("")){
+                System.out.println("Error: Invalid Input. Enter Yes or No.");
+            } else if (answer.equals("yes")) {
+                System.out.println("Restarting game.");
+                game();
+            } else if (answer.equals("no")) {
+                System.out.println("Ending game");
+                break;
+            }else {
+                System.out.println("Error: Invalid Input. Enter Yes or No.");
+            }
+        }
+    }
+
+    /**
+     * loss()
+     * This static method prints a loss message, then asks the user if they want to play again.
+     */
+    public static void loss(String secretWord){
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("You lose.");
+        System.out.printf("The word is %s%n", secretWord);
+        while(true){
+            System.out.print("Do you want to play again? Yes or no: ");
+            String answer = user_input.next();
+            answer = cleanUp(answer);
+            if (answer.equals("")){
+                System.out.println();
+                System.out.println("Error: Invalid Input. Enter Yes or No.");
+            } else if (answer.equals("yes")) {
+                System.out.println();
+                System.out.println("Restarting game.");
+                game();
+            } else if (answer.equals("no")) {
+                System.out.println();
+                System.out.println("Ending game");
+                break;
+            }else {
+                System.out.println();
+                System.out.println("Error: Invalid Input. Enter Yes or No.");
+            }
+        }
     }
 
     /**
@@ -233,11 +273,16 @@ public class ConsoleHangman {
      */
     public static String cleanUp(String user_input){
         user_input = user_input.toLowerCase();
-        return user_input.replace(" ","");
+        return user_input.replace("\\s","");
     }
 
-    public static void guessedLettersArray(ArrayList<String> guessedLettersBefore){
-        if (guessedLettersBefore.size() != 0) { // printing the guessed letters array.
+    /**
+     * guessedLettersArray()
+     * This static method prints out the guessed letters
+     * @param guessedLettersBefore
+     */
+    public static void guessedLettersArray(ArrayList<String> guessedLettersBefore){ // printing the guessed letters array.
+        if (guessedLettersBefore.size() != 0) { // if the ArrayList is empty don't print anything out.
             String guessedLettersBeforePrint = "{";
             for (int i = 0; i < guessedLettersBefore.size(); i++) {
                 if (i != guessedLettersBefore.size() - 1) {
